3dc03dbb5dc37cfc9f9a010250b1b7d17d69ece0
finnjonasrolf <office.rolf@protonmail.com>
2025-06-23 13:16:00 +0200

    ** New Features ** - Add new function "Parse_Metar" to Parse the METAR into different weather parameter. This is only the preparation for the upcoming feature. - Update and complete the function "Gen_Metar_from_file". This function now creating a list for the requested METAR and TAF and return it as different lists.
    Improvements:
    
    - The main includes parameter-wise function in the example for a better change by the user.
     - the function "get_file" now print "Connect to Ogimet..", when a request is made by the user and not only when the function is used. Add here a response/print when the file exist and the file is not generated. In any Case the function return the path of the file.

--- project.py
+++ project.py
@@ -13,11 +13,18 @@ import os
 
 from pathlib import Path
 
+import xarray as xr
+
+import pandas as pd
+
+
 def Generate_request(icao:str,name:str,auto:bool,
         year:int,month:int,day:int,hour:int,minute:int,
         year_f:int,month_f:int,day_f:int,hour_f:int,minute_f:int
     ) -> str:
+
         #Set conditions...
+
         base_url = 'https://www.ogimet.com/display_metars2.php?'
         lang = 'en'    #Language
         tipo = 'ALL'
@@ -25,6 +32,7 @@ def Generate_request(icao:str,name:str,auto:bool,
         nil = 'SI'                             # INCLUDE NIL-Messages
         fmt = 'txt'                            # File-Format from OGIMET
         send = 'send'
+
         #Create URL
         url = (base_url+'lang='+lang+'&lugar='+icao
                +'&tipo='+tipo+'&ord='+ord+'&nil='+nil+'&fmt='+fmt
@@ -42,11 +50,11 @@ def Generate_request(icao:str,name:str,auto:bool,
 def Get_file(url:str,icao:str,
              year:int,month:int,day:int,hour:int,minute:int,
              year_f:int,month_f:int,day_f:int,hour_f:int,minute_f:int
-    ) -> str:
+    ) -> Path:
     ''' This Function gets the requested METAR/TAF file in the txt-format from OGIMET and download it to reduce
     unwanted requests, if you test with the same file.'''
 
-    print("Try Connect to Ogimet.. This might take a while...")
+    print("Start to generate File")
 
     d_path = (icao
               + f'{year}' + '_'
@@ -64,12 +72,11 @@ def Get_file(url:str,icao:str,
 
     a_path = Path(d_path)
 
-
     if os.path.exists(a_path) == False:
+        print("Try Connect to Ogimet.. This might take a while...")
         connect_to_url = request.urlopen(url)
         url_status = connect_to_url.code
         if url_status == 200:
-            print('Download file')
             urllib.request.urlretrieve(url, a_path)  # This Command save the file into the folder...
             print('Download finished!')
             return a_path
@@ -78,33 +85,219 @@ def Get_file(url:str,icao:str,
             print(url_status)
             return a_path
     else:
-        print('File already exists! No Download...')
+        print('File already exists! No new File was generated!')
         return a_path
 
-def Gen_Metar_from_file(path:str):
-    ''' This Function will generate the METAR & TAF from the OGIMET-File and return a METAR-LIST and TAF-LIST '''
+def Gen_Metar_from_file(path:Path):
+    ''' This Function will generate the METAR & TAF
+    from the OGIMET-File and return a METAR-LIST and TAF-LIST '''
+    metar_list = []
+    taf_list = []
+    continue_taf = False
 
     with open(path,'r') as afile:
           for line in afile:
-            if '#' in line[0]:
-                print(line)
+            if 'Time interval:' in line:
+                date_intervall = line
+            if 'Latitude:' in line:
+                latitude = line
+            if 'Longitude:' in line:
+                longtude = line
+            if 'Altitude:' in line:
+                altitude = line
+            if 'EDDW' in line:
+                icao = line
+            if 'METAR' in line and 'SPECI' not in line:
+                metar_list.append(line)
+            if 'SPECI' in line and 'METAR' not in line:
+                metar_list.append(line)
+            if 'TAF' in line and 'large' not in line and 'short' not in line:
+                taf_list.append(line)
+                continue_taf = True
+            elif continue_taf == True and '=' not in line:
+                taf_list.append(line)
+            elif continue_taf == True and '=' in line:
+                taf_list.append(line)
+                continue_taf = False
+
+    return metar_list, taf_list
+
+def Parse_Metar(metar_list):
+    # INITS
+    '''This function using the METAR list to get the weather observation, divide it into different parts.'''
+    wind_unit = ['KT', 'MPS', 'KMH']
+
+    wind_list = []
+    date_list = []
+    var_wind_list = []
+    vis_list = []
+    cloud_list_1 = []
+    cloud_list_2 = []
+    cloud_list_3 = []
+    special_cloud_list = []
+    temperature_list = []
+    dewpoint_list = []
+    sig_weather_list = []
+    pressure_list = []
+    trend_list = []
+
+    auto_mode = False
+    cavok_mode = False
+    wind_mode = False
+    latest_idx = 0
+    got_wind = -99
+    got_wind_var = -99
+    got_cloud_1 = -99
+    got_cloud_2 = -99
+    got_cloud_3 = -99
+    got_special_cloud = -99
+    got_sig_weather = -99
+    got_vis = -99
+    got_temp = -99
+    got_pressure = -99
+    got_trend = -99
+
+    i = 0
+
+    for metar in metar_list:
+
+        # print(i,metar)
+        i = i + 1
+        # date = metar[0:13]
+        date = datetime(int(metar[0:4]),
+                        int(metar[4:6]),
+                        int(metar[6:8]),
+                        int(metar[8:10]),
+                        int(metar[10:13])
+                        )
+        date_list.append(date)
+
+        #print(metar)
+
+        if 'AUTO' in metar:
+            auto_mode = True
+        for unit in wind_unit:
+            if unit in metar:
+                wind_mode = unit
+                break
+        if 'CAVOK' in metar:
+            cavok_mode = True
+
+        idx_list = []
+
+        for idx in range(13, len(metar)):
+            if metar[idx] == ' ':
+                idx_list.append(idx)
+                #print(metar[latest_idx:idx])
+
+                if cavok_mode == True:
+                    if (got_wind>0 and 'V' not in metar[got_wind:got_wind+5]
+                            and got_vis<0) :
+                        vis_list.append('CAVOK')
+                        cloud_list_1.append('CAVOK')
+                        cloud_list_2.append('CAVOK')
+                        cloud_list_3.append('CAVOK')
+                        special_cloud_list.append('CAVOK')
+                        sig_weather_list.append('CAVOK')
+                        got_vis = idx
+                        got_sig_weather = idx
+                        got_cloud_1 = idx
+                        got_cloud_2 = idx
+                        got_cloud_3 = idx
+                        got_special_cloud = idx
+                        got_sig_weather = idx
+                else:
+                    if got_wind>0 and 'V' not in metar[got_wind:got_wind+5]:
+                        vis_list.append(metar[latest_idx:idx])
+
+                if (got_wind>0
+                        and 'V' in metar[got_wind:got_wind+5]
+                        and got_wind_var<0
+                ):
+                    if cavok_mode == False:
+                        var_wind_list.append(metar[latest_idx:idx])
+                        got_wind_var = idx
+                    else:
+                        if 'CAVOK' in metar[got_wind:got_wind+6]:
+                            var_wind_list.append('-99V-99')
+                            got_wind_var = idx
+                        else:
+                            var_wind_list.append(metar[latest_idx:idx])
+                            got_wind_var = idx
 
 
+                if wind_mode in metar[latest_idx:idx] and got_wind<0:
+                    wind_list.append(metar[latest_idx:idx])
+                    got_wind = idx
+
+                latest_idx = idx
+
+        if got_wind_var<0:
+            var_wind_list.append('-99V-99')
+
+        got_wind = -99
+        got_wind_var = -99
+        got_cloud_1 = -99
+        got_cloud_2 = -99
+        got_cloud_3 = -99
+        got_special_cloud = -99
+        got_sig_weather = -99
+        got_vis = -99
+        got_temp = -99
+        got_pressure = -99
+        got_trend = -99
+        auto_mode = False
+        cavok_mode = False
+        #print(metar[latest_idx:])
+
+    print(wind_list)
+    print(date_list)
+    print(var_wind_list)
+
+    #ds = xr.Dataset(
+    #    data_vars=dict(wind_list,var_wind_list),
+    #    dims=["time"],
+    #    coords=dict(time=date_list),
+    #    attrs=dict(description='windspeed',
+    #               units=f'{wind_mode}'
+    #               )
+    #    )
+
+    #print(ds)
 
 date = datetime.now()
 date2 = datetime.now(timezone.utc)
 print(date.strftime('%a %d %b %Y %H:%M'))
 print(date2.strftime('%a %d %b %Y %H:%M'))
 
-url = Generate_request('EDDW','Bremen',True,
-                               2025,6,7,11,0,
-                               2025,6,8,9,59
-                               )
+flugplatz = 'EDDW'
+stadt = 'Bremen'
+automat = True
+year = 2025
+month = 6
+day = 7
+hour = 11
+minute = 0
+year_f = 2025
+month_f = 6
+day_f = 8
+hour_f = 9
+minute_f = 59
 
-path = Get_file(url,'EDDW',2025,6,7,11,0,
-                               2025,6,8,9,59)
+url = Generate_request(flugplatz,stadt,automat,
+                       year,month,day,hour,minute,
+                       year_f,month_f,day_f,hour_f,minute_f
+                       )
 
-Gen_Metar_from_file(path)
+path = Get_file(url,flugplatz,
+                year,month,day,hour,minute,
+                year_f,month_f,day_f,hour_f,minute_f
+                )
+
+metar,taf = Gen_Metar_from_file(path)
+
+Parse_Metar(metar)
+
 
 # FX,FF,DD
 # DD > 60° wenn ff >= 5 KT
